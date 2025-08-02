6457169aba360c80248cdd00b0e1b9a42f626e5f
yushi <yusufshihata2006@gmail.com>
2025-08-02 11:28:09 +0300

    connecting cpp backend to python api and apply the >> operator

---
+++ nawah/__init__.py
@@ -0,0 +1,19 @@
+# 1. Import your final, user-facing Tensor class from its specific location.
+from .python.core.tensor import Tensor
+
+from . import _C
+
+DType = _C.DType
+Device = _C.Device
+DeviceType = _C.DeviceType
+Tape = _C.Tape
+cuda_synchronize = _C.cuda_synchronize
+
+__all__ = [
+    "Tensor",
+    "DType",
+    "Device",
+    "DeviceType",
+    "Tape",
+    "cuda_synchronize",
+]
