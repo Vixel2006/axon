f6a7cf8af84291649b501ef8c2beed5948df1219
yushi <yusufshihata2006@gmail.com>
2025-09-01 18:40:00 +0300

    conv layer working

--- py/functions.py
+++ py/functions.py
@@ -5,6 +5,13 @@ from py.ops.mop import *
 from py.ops.rop import *
 from py.elnawah_bindings.c_wrapper_functions import c_zeros, c_ones, c_randn, c_uniform
 
+# =========== Initialization Operations ============
+def zeros(shape: tuple[int, ...] | list[int], requires_grad: bool = True) -> Tensor: return Tensor(_c_tensor_ptr=c_zeros(shape, len(shape), requires_grad))
+def ones(shape: tuple[int, ...] | list[int], requires_grad: bool = True) -> Tensor: return Tensor(_c_tensor_ptr=c_ones(shape, len(shape), requires_grad))
+def randn(shape: tuple[int, ...] | list[int], requires_grad: bool = True, seed: int = 42) -> Tensor: return Tensor(_c_tensor_ptr=c_randn(shape, len(shape), requires_grad))
+def uniform(shape: tuple[int, ...] | list[int], requires_grad: bool = True, low: int = 0, high: int = 1) -> Tensor:
+    return Tensor(_c_tensor_ptr=c_uniform(shape, len(shape), low, high, requires_grad))
+
 # =========== Unary Operations =============
 def relu(a: Tensor) -> Tensor: return ReLU.create_node(a)
 def log(a: Tensor) -> Tensor: return Log.create_node(a)
@@ -15,6 +22,10 @@ def neg(a: Tensor) -> Tensor: return Neg.create_node(a)
 # =========== Binary Operations =============
 def add(a: Tensor | float, b: Tensor | float) -> Tensor: return Add.create_node(a, b)
 def mul(a: Tensor | float, b: Tensor | float) -> Tensor: return Mul.create_node(a, b)
+def matmul(a: Tensor, b: Tensor) -> Tensor: return MatMul.create_node(a, b)
+
+def conv2d(a: Tensor, b: Tensor, kernel_size: tuple[int, ...], stride: tuple[int, int], padding: int) -> Tensor:
+    return Conv2D.create_node(a, b, kernel_size=kernel_size, stride=stride, padding=padding)
 
 def sub(a: Tensor | float, b: Tensor | float) -> Tensor:
     if isinstance(a, Tensor):
@@ -39,3 +50,7 @@ def transpose(a: Tensor, n: int, m: int) -> Tensor: return Transpose.create_node
 def sum(a: Tensor, dim: int | None = None, keepdim: bool = False) -> Tensor: return Sum.create_node(a, dim=dim, keepdim=keepdim)
 def mean(a: Tensor, dim: int | None = None, keepdim: bool = False) -> Tensor: return Mean.create_node(a, dim=dim, keepdim=keepdim)
 def max(a: Tensor, dim: int | None = None, keepdim: bool = False) -> Tensor: return Max.create_node(a, dim=dim, keepdim=keepdim)
+
+if __name__ == "__main__":
+    a = Tensor((1, 1, 2, 3), [[[[3,4,5], [2,3,4]]]])
+    b = Tensor((2, 2, 2), [[[1,2],[1,2]], [[1,2],[1,2]]])
---
+++ py/nn/conv.py
@@ -0,0 +1,43 @@
+from .module import Module
+from py.core.tensor import Tensor
+from py.functions import zeros, conv2d
+from .init import *
+
+class Conv2d(Module):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: tuple[int, ...] | list[int],
+        stride: tuple[int, int] | int = (1, 1),
+        padding: int = 0,
+        bias: bool = False
+    ):
+        self.kernel_size = (in_channels, out_channels, *kernel_size)
+        self.weights = xavier_normal_(self.kernel_size, in_features=in_channels, out_features=out_channels)
+        self.bias = None
+
+
+        self.stride = stride if isinstance(stride, tuple) else (stride, stride)
+        self.padding = padding
+
+        if bias:
+            self.bias = zeros((out_channels))
+
+    def forward(self, x: Tensor) -> Tensor:
+        out = conv2d(x, self.weights, self.kernel_size, self.stride, self.padding)
+
+        if self.bias:
+            out += self.bias
+        
+        return out
+
+if __name__ == "__main__":
+    x = Tensor((1,1,4,4), [[[1,2,3,4], [1,2,3,4], [1,2,3,4], [1,2,3,4]]])
+    layer = Conv2d(in_channels=1, out_channels=2, kernel_size=(2,2))
+
+    pred = layer(x)
+
+    pred.realize()
+
+    print(pred)
