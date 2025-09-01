e92a6cafd67ddb52c5ae141fbcaba662101b9d97
yushi <yusufshihata2006@gmail.com>
2025-09-01 19:37:46 +0300

    conv2d backward works, but still with stride=1, padding=0

--- py/nn/init.py
+++ py/nn/init.py
@@ -4,18 +4,18 @@ from py.core.tensor import Tensor
 
 def xavier_uniform_(shape: tuple[int, ...], in_features: int, out_features: int) -> Tensor:
     bound = math.sqrt(6 / in_features + out_features)
-    return uniform(shape, -bound, bound)
+    return uniform(shape, low=-bound, high=bound)
 
 def xavier_normal_(shape: tuple[int, ...], in_features: int, out_features: int) -> Tensor:
     lower_bound = 0
     upper_bound = 2 / (in_features + out_features)
-    return uniform(shape, lower_bound, upper_bound)
+    return uniform(shape, low=lower_bound, high=upper_bound)
 
 def kaiming_uniform_(shape: tuple[int, ...], in_features: int) -> Tensor:
     bound = math.sqrt(6 / in_features)
-    return uniform(shape, -bound, bound)
+    return uniform(shape, low=-bound, high=bound)
 
 def kaiming_normal_(shape: tuple[int, ...], in_features: int) -> Tensor:
     lower_bound = 0
     upper_bound = 2 / in_features
-    return uniform(shape, lower_bound, upper_bound)
+    return uniform(shape, low=lower_bound, high=upper_bound)
