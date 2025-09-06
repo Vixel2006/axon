ae24a986b35555e0dca744115f10b2f2e21ee8c9
yushi <yusufshihata2006@gmail.com>
2025-09-06 14:14:58 +0300

    adding dot product and parameter and buffer accessing

--- idrak/nn/module.py
+++ idrak/nn/module.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from idrak.core.tensor import Tensor
 from typing import Any
 
 class Module(ABC):
@@ -8,3 +9,25 @@ class Module(ABC):
 
     def __call__(self, x, *args: Any, **kwds: Any) -> Any:
         return self.forward(x, *args, **kwds)
+
+    @property
+    def params(self):
+        params_list = []
+        for _, value in self.__dict__.items():
+            if isinstance(value, Tensor):
+                if value.requires_grad:
+                    params_list.append(value)
+            elif isinstance(value, Module):
+                params_list.extend(value.params)
+        return params_list
+
+    @property
+    def buffers(self):
+        buffers_list = []
+        for _, value in self.__dict__.items():
+            if isinstance(value, Tensor):
+                if not value.requires_grad:
+                    buffers_list.append(value)
+            elif isinstance(value, Module):
+                buffers_list.extend(value.buffers)
+        return buffers_list
