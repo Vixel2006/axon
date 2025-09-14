8d4b235d7abca6c6bbba921520b068ecd2d23965
yushi <yusufshihata2006@gmail.com>
2025-09-14 12:18:49 +0300

    finishing cpu ops

---
+++ include/ops/reduction_ops.h
@@ -0,0 +1,14 @@
+#ifndef IDRAK_REDUCTION_OPS_H
+#define IDRAK_REDUCTION_OPS_H
+
+#include "init_ops.h"
+#include "tensor.h"
+
+void sum_op(Tensor* a, Tensor* b, int axis, bool keepdim);
+void mean_op(Tensor* a, Tensor* b, int axis, bool keepdim);
+void max_op(Tensor* a, Tensor* b, int axis, bool keepdim);
+void sum_full_op(Tensor* a, Tensor* out);
+void mean_full_op(Tensor* a, Tensor* out);
+void max_full_op(Tensor* a, Tensor* out);
+
+#endif
