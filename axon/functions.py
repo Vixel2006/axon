ead0d13a44e8de6d2e30083c00ba2f62a1616894
Vixel2006 <yusufshihata2006@gmail.com>
2025-10-11 11:43:11 +0300

    finishing the full reduction grad kernels on cuda

--- axon/functions.py
+++ axon/functions.py
@@ -119,13 +119,13 @@ def max(a: Tensor, dim: int | None = None, keepdim: bool = True) -> Tensor: retu
 
 if __name__ == "__main__":
     a = from_data((2, 2, 2), [[[1, 2], [3, 5]], [[2, 6], [3, 4]]], device="cuda")
-    b = from_data((2,2,2), [[[1,2], [3,5]], [[2,6], [3,4]]], device="cuda")
+    #b = from_data((2,2,2), [[[1,2], [3,5]], [[2,6], [3,4]]], device="cuda")
 
-    c = a @ b
+    c = max(a)
 
     c.backward()
 
 
     print(c)
     print(a.grad)
-    print(b.grad)
+    #print(b.grad)
