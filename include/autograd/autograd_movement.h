a0ae8f57dbc8169b73f39110ebbffd2552761f32
Vixel2006 <yusufshihata2006@gmail.com>
2025-10-16 09:29:54 +0300

    finishing the concat grad functions on cpu and cuda backends

--- include/autograd/autograd_movement.h
+++ include/autograd/autograd_movement.h
@@ -12,6 +12,12 @@
 extern "C"
 {
 #endif
+
+    typedef struct
+    {
+        int axis;
+    } ConcatExtras;
+
     AXON_EXPORT void concat_grad_op_cpu(Tensor* out, Tensor** prev, int n_prev, void* extras);
 
     AXON_EXPORT void concat_grad_op_cuda(Tensor* out, Tensor** prev, int n_prev, void* extras);
